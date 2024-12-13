4de6fc455b9af345e5b9bd5e6d3574792ad0295a
tuannguyenquang <lakerman882@gmail.com>
2024-12-13 11:11:59 +0700

    Update Do_an.py

--- Do_an.py
+++ Do_an.py
@@ -1,3 +1,14 @@
 import tkinter as tk
 import pandas as pd
 
+# Đọc file CSV với encoding phù hợp
+file_path = 'laptop_price.csv'
+data = pd.read_csv(file_path, encoding='ISO-8859-1')
+
+# Trích xuất cột laptop_ID
+laptop_ids = data['Company']
+loai_laptop= data["Product"]
+cpu_laptop= data["Cpu"]
+ram_laptop = data["Ram"]
+memory_laptop = data["Memory"]
+card_dohoa_laptop = data["Gpu"]
