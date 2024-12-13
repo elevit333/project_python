a70c1544dd86421657aa0c7b5ad3590d1350727a
Đồ án laptop_price <vmcuong231@gmail.com>
2024-12-13 11:51:35 +0700

    Update Do_an.py

--- Do_an.py
+++ Do_an.py
@@ -6,9 +6,21 @@ file_path = 'laptop_price.csv'
 data = pd.read_csv(file_path, encoding='ISO-8859-1')
 
 # Trích xuất cột laptop_ID
-laptop_ids = data['Company']
-loai_laptop= data["Product"]
-cpu_laptop= data["Cpu"]
-ram_laptop = data["Ram"]
-memory_laptop = data["Memory"]
-card_dohoa_laptop = data["Gpu"]
+data_set = {'Hãng': data['Company'],
+'Product': data["Product"],
+'cpu': data["Cpu"],
+'ram': data["Ram"],
+'memory': data["Memory"],
+'GPU': data["Gpu"]}
+df = pd.DataFrame(data_set)
+#giao diện bộ lọc laptop
+def bo_loc():
+    filtered_df = df.copy()
+    for column in ['Hãng','cpu','ram','memory','GPU']:
+      selected_value = filter_vars[column].get()
+      if selected_value != 'Tất cả':
+        filtered_df = filtered_df[filtered_df[column] == selected_value]
+#lọc theo khoảng giá
+    min_price = float(min_price_entry.get()) if min_price_entry.get() else 0
+    max_price = float(max_price_entry.get()) if max_price_entry.get() else float('inf')
+    filtered_df = filtered_df[(filtered_df['Giá cả'] >= min_price) & (filtered_df['Giá cả'] <= max_price)]
